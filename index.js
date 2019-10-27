f6fa4e1a2d372105660249ef48c18bc4643ef0dc
theboys619 <theboys619@gmail.com>
2019-10-26 21:43:47 -0400

    init

--- index.js
+++ index.js
@@ -274,6 +274,6 @@ io.on('connection', function (socket) {
 
 });
 
-server.listen(port, function(){
+server.listen((process.env.PORT || port, function(){
   console.log('listening on *:' + port);
 });
